f4c2721382f3433c93f01c815f5d967085b73319
Abdul El-Saied <abdul.elsaied@berkeley.edu>
2021-12-28 16:28:18 -0800

    add 3sum and matrix_zeroes

--- leetcode_medium_set.py
+++ leetcode_medium_set.py
@@ -1,20 +1,44 @@
-### contains the Leetcode Medium set
+def three_sum(nums):
+    """ Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
 
-def threeSum(nums):
+    Approach 1 (⭐):
+    - Sort the array 
+    - For each element nums[i], iterate over the rest of the array and find the 2Sums, using the two pointer approach. 
+    - If nums[i] > 0, it can't be the start of a new triple, and check for duplicates since they are adjacent
+    - For two pointer 2Sum, iterate left after finding a triple to pass over duplicates
+    - Runtime: O(n^2) --> O(n) for 2Sum done for n elements
+    - Space: O(logn)/O(n) --> space required for Quicksort
+
+    Approach 2: 
+    - Sort the array 
+    - For each element nums[i], iterate over the rest of the array and find the 2Sums, using the hash approach.
+    - If nums[i] > 0, it can't be the start of a new triple, and check for duplicates since they are adjacent
+    - For hash 2Sum, pass in the rest of the array (nums[i + 1:]) and find two numbers that sum to  -nums[i]
+    - Runtime: O(n^2) --> same as approach 1
+    - Space: O(n) --> space used by hashset
+
+    Approach 3:
+    - Do not sort the array
+    - Use a hashset to skip over duplicates in the outer loop.
+    - Iterate over the rest of the list, checking if the complement is in a hashmap, and the value corresponds to the current index
+    - This allows you to reuse one hashmap.
+    - Runtime: O(n^2) --> same as approach 1
+    - Space: O(n) --> space used by hashset/hashmap
+    """
     result = []
     length = len(nums)
     if length < 3:
         return result
-    nums.sort() ## nlogn time, 
+    nums.sort() ## nlogn time, logn space for quicksort
     for i in range(length):
         element = nums[i]
         if element > 0:
             break
         if i == 0 or nums[i - 1] != element:
-            twoSum(nums, i, result)
+            two_sum(nums, i, result)
     return result
                 
-def twoSum(nums, i, result):
+def two_sum(nums, i, result):
     left, right = i + 1, len(nums) - 1
     while left < right:
         current_sum = nums[i] + nums[left] + nums[right]
@@ -28,4 +52,52 @@ def twoSum(nums, i, result):
             right -= 1
             while left < right and nums[left] == nums[left - 1]:
                 left += 1
-    
+
+###########################################################################################################################
+###########################################################################################################################
+
+def set_zeroes(matrix) -> None:
+    """ Given an m x n integer matrix matrix, if an element is 0, set its entire row and column to 0's, and return the matrix.
+
+    Do not return anything, modify matrix in-place instead.
+
+    Approach 1:
+    - Loop through the entire matrix, noting the row and column of every element that is 0 in 2 sets.
+    - On the next iteration of the matrix, if the element shares a row or column with the previously stored r/c, set to 0
+    - Runtime: O(M x N) --> iterating through the entire M x N matrix
+    - Space: O(M + N) --> space for the row and col sets
+
+    Approach 2 (⭐):
+    - Use the first cell of each row/col as a flag for the entire row/col
+    - In the case of the first col, if there was a 0 contained in matrix[i][0], then that column should be 0'ed out (is_col = True).
+    - In the case of the first row, if there was a 0 contained in matrix[0][j], then that row should be 0'ed out (matrix[0][0] = 0).
+    - Runtime: O(M x N) --> iterating through the entire M x N matrix
+    - Space: O(1) --> no extra space used, except 1 variable is_col
+    """
+    is_col = False
+    row_length = len(matrix)
+    col_length = len(matrix[0])
+    for i in range(row_length):
+        if matrix[i][0] == 0:
+            is_col = True
+        for j in range(1, col_length):
+            if matrix[i][j] == 0:
+                matrix[0][j] = 0 # flag for which columns should be 0'ed out 
+                matrix[i][0] = 0 # flag for which rows should be 0'ed out         
+                
+    for i in range(1, row_length):
+        for j in range(1, col_length):
+            if not matrix[i][0] or not matrix[0][j]:
+                matrix[i][j] = 0
+
+    if matrix[0][0] == 0:
+        for j in range(col_length):
+            matrix[0][j] = 0
+            
+    if is_col:
+        for i in range(row_length):
+            matrix[i][0] = 0
+
+###########################################################################################################################
+###########################################################################################################################
+
