943ebf98945d930d81734682e13925ad1c275c97
Abdul El-Saied <abdul.elsaied@berkeley.edu>
2022-01-05 11:06:49 -0800

    add longest palindrome

--- leetcode_medium_set.py
+++ leetcode_medium_set.py
@@ -172,5 +172,35 @@ def longest_substring_no_duplicates(s):
         i += 1
     return res
 
+###########################################################################################################################
+###########################################################################################################################
+
+def longest_palindrome(s):
+    """ Given a string s, return the longest palindromic substring in s.
+
+    Approach 1: (brute force)
+    - Choose all possible start/end pairs, and check if they are palindromes
+    - Runtime: O(n^3)
+    - Space: O(1)
+    """
+    if len(s) < 1:
+        return ""
+    start = 0 
+    end = 0 
+    for i in range(len(s)):
+        len1 = expandCenter(s, i, i)
+        len2 = expandCenter(s, i, i + 1)
+        max_len = max(len1, len2)
+        if max_len > end - start:
+            start = i - (max_len - 1) // 2
+            end = i + max_len // 2
+    return s[start : end + 1]
+        
+def expandCenter(s, left, right):
+    while (left >= 0 and right < len(s) and s[left] == s[right]):
+        left -= 1
+        right += 1
+    return right - left - 1
+
 
 
